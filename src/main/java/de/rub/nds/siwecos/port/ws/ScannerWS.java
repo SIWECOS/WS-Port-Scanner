0f4763924b0f76ea3d3dcfef6040e13a8b954141
Jens Pelzetter <jens@jp-digital.de>
2021-12-11 16:48:09 +0100

    Updated log4j dependency because of CVE-2021-44228

--- src/main/java/de/rub/nds/siwecos/port/ws/ScannerWS.java
+++ src/main/java/de/rub/nds/siwecos/port/ws/ScannerWS.java
@@ -44,7 +44,8 @@ public class ScannerWS {
         LOGGER.info("Received a request to scan: " + request.getUrl());
         if (request.getCallbackurls() == null || request.getCallbackurls().length == 0) {
             LOGGER.warn("No callback urls provided");
-            return Response.status(Response.Status.BAD_REQUEST).entity("No callback urls provided").type(MediaType.TEXT_PLAIN_TYPE).build();
+            return Response.status(Response.Status.BAD_REQUEST).entity("No callback urls provided")
+                    .type(MediaType.TEXT_PLAIN_TYPE).build();
         }
         PoolManager
                 .getInstance()
